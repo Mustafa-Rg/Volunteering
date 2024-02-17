cb01d84e891a4b552e52ec9fd7b699f7d4a9c4d4
Mustafa Ergaibi <mustafaalrgaiby444@gmail.com>
2024-02-17 14:01:56 +0200

    Updated search.blade.php to give the proper post component

--- resources/views/posts/search-results.blade.php
+++ resources/views/posts/search-results.blade.php
@@ -1,5 +1,10 @@
 
 <!--Show posts-->
 @foreach($posts as $post)
-    @component('components.post-component', ['post' => $post])@endcomponent
+    @if (auth()->user()->user_type === 'organisation')
+        @component('components.post-component', ['post' => $post])@endcomponent
+    @else
+        @component('components.vol-post-component', ['post' => $post])@endcomponent
+    @endif
+    
 @endforeach
